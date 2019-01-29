f1c35828dd82768f6646a86065290bbec37d969d
Vu Nguyen <vunguyen@vus-macbook-pro.lan>
2019-01-28 22:15:37 -0800

    Added required assignments

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let w = 1
+let x = 2;
+let y = 4;
+let z = 6;
+
+console.log(x+y+z+w) // 13
+console.log(y*z*w) // 24
+console.log(z/x) // 3
+console.log(z%x*w) // 0
+console.log((w+x)/2*y) // 6
---
+++ countdown.js
@@ -0,0 +1,5 @@
+for (let i= 10; i >= 0; i--) {
+  console.log(i)
+}
+
+console.log(x); // 10 9 8 7 6 6 5 4 3 2 1 0
